bf5f22ddb3f718141c25386758db1e6f43336a10
M27IAR <zhn3354198192@gmail.com>
2024-12-05 17:31:47 +0800

    empty

--- BHB.js
+++ BHB.js
@@ -10,9 +10,6 @@
 // ==/UserScript==
 
 (function(){
-
-
-
     function addtolocal() {
         let heightsize=document.getElementById("heightsize");//高度比例
         let widthsize=document.getElementById("widthsize");//宽度比例
@@ -24,9 +21,9 @@
         let leaderhideplan=document.querySelector("#leadermanhide");//左侧菜单隐藏
         let canseenunber=document.querySelector("#notseenumber");//背景透明度（只在非backimage状态生效）
         let PrintToBackgroundValue=document.querySelector("#printToWebback");
-        let LocalBbsNameFontSize=document.querySelector("#size");
-        let LocalFontColorset=document.querySelector("#fontcolor");
-        let BorderTextSize=document.querySelector("#BorderText");
+        let LocalBbsNameFontSize=document.querySelector("#size");//聊天室名称大小输入框
+        let LocalFontColorset=document.querySelector("#fontcolor");//聊天室名称颜色
+        let BorderTextSize=document.querySelector("#BorderText");//聊天室描边大小输入框
 
 
 
@@ -177,56 +174,38 @@
         imgFile.onload = function () {
             let imgDataBase64 = this.result; //base64数据
             console.log(imgDataBase64);
-
-
             // 打开或创建一个数据库
             let request = indexedDB.open('databaseName', 4);
-
-// 处理数据库升级
+            // 处理数据库升级
             request.onupgradeneeded = function(event) {
                 let db = event.target.result;
-
                 // 创建对象存储（表）并设置主键
                 let objectStore = db.createObjectStore('storeName', { keyPath: 'id' });
-
                 // 创建索引
                 objectStore.createIndex('fieldName', 'fieldName', { unique: false });
-            };
-
-// 数据库打开成功时的回调
-            request.onsuccess = function(event) {
+            };request.onsuccess = function(event) {// 数据库打开成功时的回调
                 let db = event.target.result;
-
                 // 进行事务操作
                 let transaction = db.transaction('storeName', 'readwrite');
                 let objectStore = transaction.objectStore('storeName');
-
                 // 插入数据
                 objectStore.put({ id: 1, name:imgDataBase64});
                 transaction.oncomplete = function() {
                     console.log('数据插入DBD成功');
                 };
-
                 transaction.onerror = function(event) {
                     console.error('Transaction failed:', event);
-                };
-
+                };//准备写入图片的BASE64数据
                 let queryTransaction = db.transaction(['storeName']);
                 let queryObjectStore = queryTransaction.objectStore('storeName');
                 let query = queryObjectStore.get(1);
-
                 query.onsuccess = function(event) {
                     console.log('Customer:', event.target.result);
                     let imgdatabase64DBD=event.target.result.name;
                     console.log(imgdatabase64DBD);
-
                 };
                 db.close();
-            };
-            // 查询数据
-
-
-// 错误处理
+            };// 错误处理
             request.onerror = function(event) {
                 console.error('Database error:', event.target.error);
             };
@@ -310,21 +289,8 @@
     if(!localStorage.CantSeeColor4){//聊天页面外层边框
         localStorage.setItem("CantSeeColor4","#2b2c40");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //本地数值设定结束
+    //通过读取本地存储数据进行选择值设定
     let webpiclod="";
     let localpiclod="";
     let leadermanhide="";
@@ -343,37 +309,29 @@
     }else{
         backgroundplanone="checked"
     }
-    if (localStorage.printToBack==="1"){//背景的填充方式
+    if (localStorage.printToBack==="1"){//背景的填充位置
         PrintToBackground="checked"
     }else{
         PrintToBBSGround="checked"
     }
-    if (localStorage.scrollstyle==="1"){//背景的填充方式
+    if (localStorage.scrollstyle==="1"){//滚动条渲染
         Scrollstylex="checked"
     }else{
         Scrollstylex=""
     }
-
-
-
-
+    //通过读取本地存储数据进行选择值设定结束
     let request = indexedDB.open('databaseName', 4);
-
 // 处理数据库升级
     request.onupgradeneeded = function(event) {
         let db = event.target.result;
-
         // 创建对象存储（表）并设置主键
         let objectStore = db.createObjectStore('storeName', { keyPath: 'id' });
-
         // 创建索引
         objectStore.createIndex('fieldName', 'fieldName', { unique: false });
     };
-
 // 数据库打开成功时的回调
     request.onsuccess = function(event) {
         let db= event.target.result;
-
         // 进行事务操作
         let transaction = db.transaction('storeName', 'readwrite');
         let objectStore = transaction.objectStore('storeName');
@@ -385,25 +343,14 @@
         transaction.onerror = function(event) {
             console.error('Transaction failed:', event);
         };
-
-
-
     };
-    // 查询数据
-
-
 // 错误处理
     request.onerror = function(event) {
         console.error('Database error:', event.target.error);
     };
-
-
-    //设置初始值部分结束
-
-
-    let nowurl = window.location.href;
+    //初始值部分结束
+    let nowurl = window.location.href;//读取当前所在网页
     if (nowurl.includes('https://boyshelpboys.com/chat.htm')) {//如果当前网页为聊天室页面
-
         if (localStorage.leaderhide==="1"){//左侧导航栏的隐藏与显示
             leadermanhide="checked"
             let DIV1=document.querySelector("#top > div")
@@ -414,8 +361,6 @@
         }else{
             leadermanhide=""
         }
-
-
     console.log(nowurl);
     let bac=document.querySelector("body")//网页本体
     let backb=document.querySelector("#top > div > div")//自顶栏往下部分
@@ -431,28 +376,19 @@
     //id是webimgsrc的是离线文件 写错了
     if (localStorage.heightsize==="auto"||localStorage.heightsize==="NaN"){
         localHightSize="auto"
-        console.log('height做到这部分了');
     }else{
         localHightSize=localStorage.heightsize+'%'
-        console.log('height做到那一个部分了');
     }
-
     if (localStorage.widthsize==="auto"||localStorage.widthsize==="NaN"){
         localWidthSize="auto"
-        console.log('width做到这部分了');
     }else{
         localWidthSize=localStorage.widthsize+'%'
-        console.log('width做到那一个部分了');
     }
-
-
     let addHTML='<div id="backread">'
     let addbott="<button class='fuckyou'>背景文件/渲染</button><button class='fuckyou'>控件透明度</button>"
     let addmain=`<div id="localsett"><div><button id="exit">X</button><span>背景图片/渲染设置</span><div><form name='myform' action='chat.htm'><input type='file' id='webimgsrc' accept='image/*'><br> <span>线上地址</span><input type='text'  value='${localStorage.webimgsrc}' name='' id='localimgsrc' width='150px'><br><span>删除左侧导航栏</span><input type="checkbox" ${leadermanhide} value="1" name="leadermanhide" id="leadermanhide"><br><span>聊天室名称大小</span><input type='text' onblur='if(!((/[(0-9)]/).test(value)))value=18' value='${localStorage.NameFontSize}' name='size' id='size' size="5"><br><span>聊天室名称描边颜色</span><input type="color" id="fontcolor" value="${localStorage.LocalFontColor}"><br><span>聊天室名称描边大小</span><input type="text" onblur='if(!((/[(0-9)]/).test(value)))value=1' size="2"  value="${localStorage.BorderTextSize}"  name="BorderText" id="BorderText"><br><span>顶部</span><input type='text' oninput='if(!((/[(0-9)/-]/).test(value)))value=0' value='${localStorage.top}' name='topp' id='top' size="5"> <span>左部</span><input type='text' oninput='if(!((/[(0-9)/-]/).test(value)))value=0' value='${localStorage.left}' name='leftt' id='left' size="5"><br> <span>背景高度比例(填写0即为auto)</span><input type='text' onblur='if(!((/[(0-9)]/).test(value))&&value!=="auto")value=100' min='0'  value='${localStorage.widthsize}' name='' id='widthsize' size='5'><br><span>背景宽度比例(填写0即为auto)</span><input type='text' onblur='if(!((/[(0-9)]/).test(value))&&value!=="auto")value=100'  min='0' value=${localStorage.heightsize} name='' id='heightsize' size="5"><br> <span>在线图片</span><input type='radio' ${webpiclod} name='picloadsele' id='webpicon'  width='100px'><span>本地图片</span><input type='radio'  name='picloadsele' ${localpiclod} id='localpicon'  width='100px'><br><hr><span>使用section写入背景只能在最底层</span><br><span>section写入</span><input type='radio'  ${sectionplanone} name='addplan' id='sectionplan'  width='100px'><br><span>body-background写入</span><input type='radio'  name='addplan' ${backgroundplanone} id='backgroundplan'  width='100px'><br><span>透明度</span><input type="range" min="0" max="1" step="0.01" value="${localStorage.canseenunber}" id="notseenumber"><hr><span>渲染到网页背景</span><input type='radio' value='1' ${PrintToBackground} name='baklocal' id='printToWebback'  width='100px'><br><span>渲染到聊天室背景</span><input type='radio' value='1' name='baklocal' ${PrintToBBSGround} id='printToBBS'  width='100px'><br> <button id='save'>提交</button></form></div>`
     let addmain2=`<div><button id="secexit">X</button><span>控件透明度设置</span><br><input type="checkbox"  ${Scrollstylex} name="ScrollSett" id="ScrollSett">滚动条控制</input><br><span>历史记录1透明度</span><input type="range" value="${localStorage.CantSeeset1}"><input type="color" value="${localStorage.CantSeeColor1}"><br><span>顶栏下部透明度</span><input type="range" value="${localStorage.CantSeeset2}"><input type="color" value="${localStorage.CantSeeColor2}"><br><span>历史记录2透明度</span><input type="range" value="${localStorage.CantSeeset3}"><input type="color" value="${localStorage.CantSeeColor3}"><br><span>外层边框透明度</span><input type="range" value="${localStorage.CantSeeset4}"><input type="color" value="${localStorage.CantSeeColor4}"><br><input type="submit" id="secsubint"><br><button id="reall">重置颜色|透明度配置</button></div>`
     let addscript="<script src='chrome-extension://jinjaccalgkegednnccohejagnlnfdag/BHB%E8%83%8C%E6%99%AF%E5%9B%BE%E7%89%87%E6%9B%B4%E6%8D%A2.user.js#13'></script>"
-
-
 
     bac.insertAdjacentHTML("afterbegin",addHTML);//插入section 填充背景
     oldaddtarge.insertAdjacentHTML("afterbegin",addbott);
@@ -460,21 +396,14 @@
     bac.insertAdjacentHTML("afterbegin",addmain2);
     bac.insertAdjacentHTML("afterbegin",addscript);
 
-        let NeedFixStyle=document.querySelectorAll("body > style");//修改滚动条状态
-        console.log(NeedFixStyle[NeedFixStyle.length-1]);
-        console.log(NeedFixStyle.length);
-        NeedFixStyle[NeedFixStyle.length-1].insertAdjacentHTML("afterend",'<style></style>');
-        let nedAddStyle=document.createTextNode(`small{-webkit-text-stroke: ${localStorage.BorderTextSize}px ${localStorage.LocalFontColor};font-size:${localStorage.NameFontSize};}  .fuckyou{background-color: #2b2c4030; color: gray; margin:5px 5px;padding: 10px 5px;border: 1px gray solid;cursor: pointer;transition: background-color 0.3s;border-radius: 10px;}.fuckyou:hover{background-color: #FFFFFF; color:#66ccff;} @media(min-width: 768px) {::-webkit-scrollbar {width: 0px; height: 6px;border-radius: 20px;}::-webkit-scrollbar-track { background: var(--bs-body-bg);}::-webkit-scrollbar-thumb {background: var(--bs-secondary);border-radius: 20px;}::-webkit-scrollbar-thumb:hover {background: var(--bs-primary);}::-webkit-scrollbar-thumb:focus, ::-webkit-scrollbar-thumb:active {background: var(--bs-primary-active);}}`)
-        let FixStyle=NeedFixStyle[NeedFixStyle.length-1].nextElementSibling
-        console.log(FixStyle);
-
+    let NeedFixStyle=document.querySelectorAll("body > style");//修改滚动条状态
+    NeedFixStyle[NeedFixStyle.length-1].insertAdjacentHTML("afterend",'<style></style>');
+    let nedAddStyle=document.createTextNode(`small{-webkit-text-stroke: ${localStorage.BorderTextSize}px ${localStorage.LocalFontColor};font-size:${localStorage.NameFontSize};}  .fuckyou{background-color: #2b2c4030; color: gray; margin:5px 5px;padding: 10px 5px;border: 1px gray solid;cursor: pointer;transition: background-color 0.3s;border-radius: 10px;}.fuckyou:hover{background-color: #FFFFFF; color:#66ccff;} @media(min-width: 768px) {::-webkit-scrollbar {width: 0px; height: 6px;border-radius: 20px;}::-webkit-scrollbar-track { background: var(--bs-body-bg);}::-webkit-scrollbar-thumb {background: var(--bs-secondary);border-radius: 20px;}::-webkit-scrollbar-thumb:hover {background: var(--bs-primary);}::-webkit-scrollbar-thumb:focus, ::-webkit-scrollbar-thumb:active {background: var(--bs-primary-active);}}`)
+    let FixStyle=NeedFixStyle[NeedFixStyle.length-1].nextElementSibling
         if (localStorage.scrollstyle==="1"){//背景的填充方式
-            console.log(FixStyle.innerHTML);
             FixStyle.appendChild(nedAddStyle);
-            console.log(FixStyle.innerHTML);
         }
         function SetScroll() {//滚动条样式调整
-            console.log("到了到了")
             if (FixStyle.innerHTML==""){
                 localStorage.setItem("scrollstyle","1")
                 FixStyle.appendChild(nedAddStyle);
@@ -486,9 +415,7 @@
 
     let addtarge=document.querySelector("#backread");
     let addbutt=document.querySelector("#navbar-collapse > button");
-    console.log(addbutt);
     let addbutt2=document.querySelector("#navbar-collapse > button:nth-child(2)");
-    console.log(addbutt2);
     let adddiv=document.querySelector("body > div:nth-child(3)");
     let adddiv2=document.querySelector("body > div:nth-child(2)");
     let addlocalupdate=document.querySelector("#webimgsrc");
@@ -503,33 +430,25 @@
     histor.setAttribute('style', `background-color: ${localStorage.CantSeeColor4}${localStorage.CantSeeset4};`)//聊天页面外层边框
 
 //bac.setAttribute('style',"background-color: #00000070;background-image:url('https://t1-img.233213.xyz/2024/11/25/67447535ec930.jpg');background-repeat=no-repeat")//背景图片替换//背景颜色
-    let openpic = indexedDB.open('databaseName', 4);//调用数据库
+    let openpic = indexedDB.open('databaseName', 4);//调用数据库读取本地存储的base64图片数据
     openpic.onupgradeneeded = function(event) {//没有调用则创建|选择了新版本数据库而重建
         let db = event.target.result;
-
         // 创建对象存储（表）并设置主键
         let objectStore = db.createObjectStore('storeName', { keyPath: 'id' });
-
         // 创建索引
         objectStore.createIndex('fieldName', 'fieldName', { unique: false });
     };
     openpic.onsuccess = function(event) {
         let db = event.target.result;
-
-
         // 查询数据
         let queryTransaction = db.transaction(['storeName']);
         let queryObjectStore = queryTransaction.objectStore('storeName');
         let query = queryObjectStore.get(1);
-
         query.onsuccess = function(event) {
             //转换透明度的数值为RGBA的数值
             let BackimagePrintPlanNum=(localStorage.canseenunber*100)%100
             if(BackimagePrintPlanNum===0&&localStorage.canseenunber==="0"){BackimagePrintPlanNum=""}
             console.log(BackimagePrintPlanNum)
-
-
-
             if(event.target.result.name==="空"){//如果数据库没有本地图片数据
             console.log('图片对象', event.target.result.name);
                 console.log('数据库无图片相关内容存储');
@@ -561,11 +480,8 @@
                             alert('数据库无图片相关内容存储，将加载在线图片，请在设置添加本地图片')//如果数据库没有本地图片数据且选择渲染本地图片
                             BBSmsgBack.setAttribute('style', `background-image: url(${localStorage.webimgsrc}); background-repeat: no-repeat; background-size:${localHightSize} ${localWidthSize}; background-position: ${localStorage.left}% ${localStorage.top}%; background-attachment:fixed;`);
                         }
-
-
                     }
                 }
-
             }else{//如果数据库有本地图片数据
                 if(localStorage.sectionplan === "1") {//用户选择方案为section方法
                     if (localStorage.webpiclod === "1") {
@@ -575,7 +491,6 @@
                         addtarge.setAttribute('style', `background-image: url('${event.target.result.name}');background-repeat: no-repeat; background-size:${localHightSize} ${localWidthSize}; display: flex; align-items: center; justify-content: center; position: fixed; top: ${localStorage.top}%; left: ${localStorage.left}%; right: 0; bottom: 0; pointer-events: none; opacity: ${localStorage.canseenunber}; z-index:0; `)
                     }
                 }else if(localStorage.backgroundplan==="1"){//用户选择方案为background方法
-
                     if (localStorage.webpiclod === "1") {
                         if(localStorage.printToBack==="1"){
                             //通过section写入项目留下的标签实现颜色覆盖
@@ -596,21 +511,15 @@
                         }else if(localStorage.printToBBS==="1"){
                             BBSmsgBack.setAttribute('style', `background-image: url(${event.target.result.name});background-repeat: no-repeat; background-size:${localHightSize} ${localWidthSize}; background-position: ${localStorage.left}% ${localStorage.top}%; background-attachment:fixed;`)
                         }
-
                     }
                 }
             }
         }
         db.close();
-
     }
-
-
-
-
     adddiv.setAttribute('style','position: absolute;top: 55%;left: 50%;transform: translate(-45%, -50%);width: 300px;height: 550px;border: 1px solid red;overflow: hidden;z-index:1;display:none;background-color:#66ccff;color:black;')
     adddiv2.setAttribute('style','position: absolute;top: 50%;left: 49%;transform: translate(-51%, -50%);width: 300px;height: 500px;border: 1px solid red;overflow: hidden;z-index:1;display:none;background-color:#66ccff;color:black;')
-    addbutt.addEventListener("click",function(){//开关设置栏
+    addbutt.addEventListener("click",function(){//开关设置栏1
         let adddiv=document.querySelector("body > div:nth-child(3)")
         if (adddiv.style.display==="block"){
             adddiv.style.display="none";
@@ -618,7 +527,7 @@
             adddiv.style.display="block";
         }
     });
-    addbutt2.addEventListener("click",function(){//开关设置栏
+    addbutt2.addEventListener("click",function(){//开关设置栏2
         let adddiv2=document.querySelector("body > div:nth-child(2)")
         if (adddiv2.style.display==="block"){
             adddiv2.style.display="none";
@@ -626,7 +535,6 @@
             adddiv2.style.display="block";
         }
     });
-
 
     let localget=document.querySelector("#save");
     let localStyleGet=document.querySelector("#secsubint")
@@ -655,7 +563,6 @@
                 histor.setAttribute('style', `background-color: ${localStorage.CantSeeColor4}${localStorage.CantSeeset4};`)//聊天页面外层边框
 
         })
-
 
         //针对@闪电炫芬批插件的外链头像图片做适配
         document.querySelector("#top > div > div > main > section > div > div > div > div.chat-history-body > ul > li:nth-child(1048)  ")
